7d31e0fd6d19ff373bfdfc4cca9309c78b92854f
suradechsamee <suradech.samee@gmail.com>
2015-04-23 12:29:05 +0200

    improve version

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 // Content toggle start //
-
 /*$(document).ready(function(){
   $("div.lol").click(function () {
     $(this).toggleClass("flipper");
@@ -13,7 +12,7 @@ $(document).ready(function(){
     return true;
   });
 });*/
-$(document).ready(function(){
+/*$(document).ready(function(){
   $(".social").mouseenter(function (e) {
       var hoverText = $(this).attr("title");
       $(".hint").text(hoverText).show();     
@@ -23,11 +22,8 @@ $(document).ready(function(){
   $(".social").mouseleave(function() {
         $(".hint").hide();
   });
-});
-
-
+});*/
 // Content toggle end //
-
 // Image slider start //
 /*
 var image1 = new Image();
@@ -40,31 +36,21 @@ var image4 = new Image();
 image4.src = "./img/gaming.jpg";
 var image5 = new Image();
 image5.src = "./img/topgear.jpg";
-
 var numberImage = 1;
 var slide = true;
-
 function slideIt() {
     if(slide)
     {
     document.images.slide.src = eval("image"+numberImage+".src")
-    
     if(numberImage < 5) {
         numberImage = numberImage + 1;
-        
         }
     else {
        numberImage = 1;
-        
     }
     }
-    setTimeout("slideIt()",5000);
-    
-    
+    setTimeout("slideIt()",5000); 
 }
-
-
-
 $(document).ready(function(){
     $("#imagis").mouseenter(function(){
         $(this).next("img").stop(true);
@@ -77,12 +63,10 @@ $(document).ready(function(){
         return true;
     });
 });
-
-
 slideIt();*/
 // Image slider end //
+/* flipper */
 function flipit(el, boo) {
-        
         if(boo == true){
         el.children[1].style.webkitTransform = "perspective(600px) rotateY(-180deg)";
         el.children[0].style.webkitTransform = "perspective(600px) rotateY(0deg)";
@@ -104,3 +88,26 @@ function flipit(el, boo) {
         el.children[0].style.webkitTransition = "all .5s linear 0s";
         }
     }
+/* flipper end */
+/* overlay */
+$(document).ready(function(){
+    $(".social").click(function(){
+        $("#overlay").fadeIn("slow");
+        $("#specialBox").show("#person");
+    });
+    $("#work1").click(function(){
+        $("#overlay").fadeIn("slow");
+        $("#specialBox2").show("#skill1");
+    });
+    $("#work2").click(function(){
+        $("#overlay").fadeIn("slow");
+        $("#specialBox3").show("#skill2");
+    });
+    $(".toggleOverlay").click(function(){
+        $("#overlay").fadeOut("slow");
+        $("#specialBox").hide();
+        $("#specialBox2").hide();
+        $("#specialBox3").hide();
+    });
+});
+/* overlay end */
